d212039a5b778797d4ee2776e582385b4c28c454
coolpeepz <colin.r.schultz@gmail.com>
2019-02-01 17:21:55 -0800

    added basic field map

---
+++ src/components/fieldmap.js
@@ -0,0 +1,46 @@
+ui.fieldmap = document.getElementById("fieldmap");
+
+function loadImage(name) {
+    let img = new Image();
+    img.src = 'images/' + name;
+    return img;
+}
+console.log('hello world')
+
+var images = {
+    map: loadImage('fieldmap.jpg'),
+    robot: loadImage('robot.png')
+};
+
+console.log('hello world2')
+
+var mapCtx = ui.fieldmap.getContext("2d");
+
+function drawImageCentered(img) {
+    mapCtx.drawImage(img, -img.width/2, -img.height/2);
+}
+
+function drawFieldMap() {
+    mapCtx.drawImage(images.map, 0, 0);
+    mapCtx.translate(robotX * 2, robotY * 2);
+    mapCtx.rotate(robotAngle);
+    drawImageCentered(images.robot);
+    mapCtx.setTransform(1, 0, 0, 1, 0, 0);
+}
+
+var robotX, robotY, robotAngle;
+NetworkTables.addKeyListener('/PositionTracking/x', (key, value) => {
+    robotX = value;
+    drawFieldMap();
+});
+
+NetworkTables.addKeyListener('/PositionTracking/y', (key, value) => {
+    robotY = value;
+    drawFieldMap();
+});
+
+NetworkTables.addKeyListener('/PositionTracking/angle', (key, value) => {
+    robotAngle = value;
+    drawFieldMap();
+});
+drawFieldMap();
