bdb87a61054a2eee14eb96a532131a4b0df1a2a1
coolpeepz <colin.r.schultz@gmail.com>
2019-02-15 18:15:11 -0800

    added vision targets

--- src/components/fieldmap.js
+++ src/components/fieldmap.js
@@ -1,9 +1,30 @@
 ui.fieldmap = document.getElementById("fieldmap");
 ui.overrideButton = document.getElementById("override");
+ui.scoreButton = document.getElementById("score");
 
 ui.overrideButton.onclick = (ev) => {
     NetworkTables.putValue('/Robot/mode', 0);
 }
+
+ui.scoreButton.onclick = (ev) => {
+    NetworkTables.putValue('/Robot/mode', 2);
+}
+
+function createVisionTarget(id, x, y) {
+    return {
+        id: id,
+        x: x,
+        y: y
+    };
+}
+
+var VISION_RADIUS = 7.5;
+
+var visionTargets = [
+    createVisionTarget(0, 0, 29.75),
+    createVisionTarget(1, 134.5, 165.25),
+    createVisionTarget(2, 148.25, 156)
+]
 
 function loadImage(name) {
     let img = new Image();
@@ -12,7 +33,7 @@ function loadImage(name) {
 }
 
 var images = {
-    map: loadImage('betafield1.png'),
+    map: loadImage('betafield2.png'),
     robot: loadImage('betarobot.png')
 };
 
@@ -23,14 +44,33 @@ function drawImageCentered(img) {
     mapCtx.drawImage(img, -img.width/2, -img.height/2);
 }
 
+function drawCircle(x, y, radius) {
+    mapCtx.beginPath();
+    mapCtx.arc(x, y, radius, 0, 2 * Math.PI);
+    mapCtx.stroke();
+}
+
 function drawFieldMap() {
     mapCtx.drawImage(images.map, 0, 0);
     let x = NetworkTables.getValue('/PositionTracking/x', 0) * 2;
     let y = NetworkTables.getValue('/PositionTracking/y', 0) * 2;
     let angle = NetworkTables.getValue('/PositionTracking/angle', 0);
+
+    let selectedTarget = NetworkTables.getValue('/Pathfinding/visionTarget', -1);
+    console.log(selectedTarget);
+    for (let i = 0; i < visionTargets.length; ++i) {
+        let vt = visionTargets[i];
+        if (vt.id == selectedTarget)
+            mapCtx.strokeStyle = "#FF0000";
+        else
+            mapCtx.strokeStyle = "#0000FF";
+        drawCircle(vt.x * 2, vt.y * 2, VISION_RADIUS * 2);
+    }
+
     let mode = NetworkTables.getValue('/Robot/mode', 0);
     if (mode != 0) {
         let path = NetworkTables.getValue('/Pathfinding/path', []);
+        mapCtx.strokeStyle = "#000000";
         mapCtx.beginPath();
         mapCtx.moveTo(x, y);
         for (let i = 0; i < path.length; i += 2) {
@@ -73,8 +113,24 @@ ui.fieldmap.onclick = (ev) => {
     let coords = ui.fieldmap.toRelativeCoords(ev);
     let x = coords[0]/2;
     let y = coords[1]/2;
-    NetworkTables.putValue('/Pathfinding/target', x + " " + y);
-    NetworkTables.putValue('/Robot/mode', 1);
+    let id = -1;
+    let r2 = VISION_RADIUS * VISION_RADIUS;
+    for (let i = 0; i < visionTargets.length; ++i) {
+        let vt = visionTargets[i];
+        let dx = x - vt.x;
+        let dy = y - vt.y;
+        if (dx * dx + dy * dy < r2) {
+            id = vt.id;
+            break;
+        }
+    }
+    if (id == -1) {
+        NetworkTables.putValue('/Pathfinding/target', x + " " + y);
+        NetworkTables.putValue('/Robot/mode', 1);
+    } else {
+        NetworkTables.putValue('/Pathfinding/visionTarget', id);
+    }
+    drawFieldMap();
 };
 
 NetworkTables.addKeyListener('/PositionTracking/x', drawFieldMap);
