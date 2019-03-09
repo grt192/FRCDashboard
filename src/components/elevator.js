8e4e0cb9e9b801a14ba7ce724816edb30a967c9a
coolpeepz <colin.r.schultz@gmail.com>
2019-03-08 17:00:15 -0800

    added elevator set buttons

--- src/components/elevator.js
+++ src/components/elevator.js
@@ -3,8 +3,15 @@ ui.elevatorButtons = [
     document.getElementById("button1"),
     document.getElementById("button2"),
     document.getElementById("button3"),
-    document.getElementById("button4"),
-];
+    document.getElementById("button4")
+]
+
+ui.elevatorSetButtons = [
+    document.getElementById("set1"),
+    document.getElementById("set2"),
+    document.getElementById("set3"),
+    document.getElementById("set4")
+]
 
 ui.elevatorButtons.forEach(element => {
     element.onmouseover = ev => {
@@ -29,4 +36,10 @@ NetworkTables.addKeyListener('/Robot/Elevator/target', (key, value) => {
         else
         element.style.background = "#505050";
     });
+});
+
+ui.elevatorSetButtons.forEach(element => {
+    element.onclick = ev => {
+        NetworkTables.putValue('/Robot/Elevator/set', element.getAttribute("value"));
+    };
 });
